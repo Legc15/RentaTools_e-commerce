0e2abed9bf60d206be5bb308ef5fca1619217276
mmir22 <marcelomiraballes@gmail.com>
2023-10-31 21:40:05 -0300

    refactor:estilo horizontal a cards

---
+++ Frontend/RentaTools/src/components/AdminTable/TrabajoIntegradorMarceloMiraballes-LeandroGarcia/TrabajoIntegrador/src/main/java/com/example/TrabajoIntegrador/dto/OdontologoDTO.java
@@ -0,0 +1,40 @@
+package com.example.TrabajoIntegrador.dto;
+
+public class OdontologoDTO {
+    private Long id;
+    private int numeroMatricula;
+    private String nombre;
+    private String apellido;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(int numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
---
+++ Frontend/RentaTools/src/components/AdminTable/TrabajoIntegradorMarceloMiraballes-LeandroGarcia/TrabajoIntegrador/src/test/java/com/example/TrabajoIntegrador/TrabajoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrabajoIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
