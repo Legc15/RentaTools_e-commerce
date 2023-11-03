ba4ef554b8f49671bc6b6394c7065b4f0ce335a1
Legc15 <leandro.garcia.casariego@gmail.com>
2023-11-03 20:48:56 -0300

    refactor: Carousel: scroll y efectos

--- Frontend/RentaTools/src/components/Carousel/index.jsx
+++ Frontend/RentaTools/src/components/Carousel/index.jsx
@@ -1,17 +1,38 @@
-import ArrowBackIos from '@material-ui/icons/ArrowBackIos';
-import ArrowForwardIos from '@material-ui/icons/ArrowForwardIos';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import PropTypes from 'prop-types';
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import "./styles.css";
 
-
 function Carousel({ imagenes, onClose }) {
-
     const carouselRef = useRef();
+    const [isAtStart, setIsAtStart] = useState(true);
+    const [isAtEnd, setIsAtEnd] = useState(false);
+
+    const checkScrollPositions = () => {
+        if (!carouselRef.current) {
+            return;
+        }
+
+        setIsAtStart(carouselRef.current.scrollLeft === 0);
+        setIsAtEnd(carouselRef.current.scrollLeft + carouselRef.current.offsetWidth >= carouselRef.current.scrollWidth - 1);
+    };
 
     const scroll = (scrollOffset) => {
-        carouselRef.current.scrollLeft += scrollOffset;
+        if (carouselRef.current) {
+            carouselRef.current.scrollLeft += scrollOffset;
+            setTimeout(checkScrollPositions, 100);
+        }
     };
+
+    useEffect(() => {
+        checkScrollPositions();
+        carouselRef.current.addEventListener('scroll', checkScrollPositions);
+        return () => {
+            if (carouselRef.current) {
+                carouselRef.current.removeEventListener('scroll', checkScrollPositions);
+            }
+        };
+    }, []);
 
     const handleClickOutside = (event) => {
         if (event.target.className === 'carousel-wrapper') {
@@ -20,36 +41,30 @@ function Carousel({ imagenes, onClose }) {
     };
 
     return (
-        <div 
-            className='carousel-wrapper' 
-            onClick={handleClickOutside}
-            >
-            <ArrowBackIos
-                className='carousel-arrow carousel-arrow-left'
-                onClick={() => scroll(-200)}
-            />
-            
-            <div className='carousel-container' ref={carouselRef} >
+        <div className='carousel-wrapper' onClick={handleClickOutside}>
+            {!isAtStart && (
+                <FaChevronLeft 
+                    className='carousel-arrow carousel-arrow-left' 
+                    onClick={() => scroll(-200)}
+                />
+            )}
+            <div className='carousel-container' ref={carouselRef}>
                 {imagenes.map((imagen, index) => (
-                    <div
-                        className='carousel-item'
-                        key={index}>
-                        <img
-                            srcSet={imagen.url}
-                            src={imagen.url}
-                            alt={index}
-                        />
-                    </div>   
+                    <div className='carousel-item' key={index}>
+                        <img srcSet={imagen.url} src={imagen.url} alt={index} />
+                    </div>
                 ))}
             </div>
-            <ArrowForwardIos
-                className='carousel-arrow carousel-arrow-right'
-                onClick={() => scroll(200)}
-            /> 
+            {!isAtEnd && (
+                <FaChevronRight 
+                    className='carousel-arrow carousel-arrow-right' 
+                    onClick={() => scroll(200)}
+                />
+            )}
         </div>
-    )
-
+    );
 }
+
 Carousel.propTypes = {
     imagenes: PropTypes.array.isRequired,
     onClose: PropTypes.func.isRequired,
