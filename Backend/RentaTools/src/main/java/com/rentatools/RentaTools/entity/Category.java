6ae8ff93b4ce0f85b3b7375403b586460bd39ae2
PabloKonig <pablokonig@yahoo.com>
2023-11-06 21:58:42 -0300

    Actualizado hasta el product/update

--- Backend/RentaTools/src/main/java/com/rentatools/RentaTools/entity/Category.java
+++ Backend/RentaTools/src/main/java/com/rentatools/RentaTools/entity/Category.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Category {
     @Id
     @SequenceGenerator(name = "category_sequence", sequenceName = "category_sequence", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_sequence")
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "category_sequence")
     private Long id;
     @Column(nullable = false, unique = true)
     private String name;
--- Backend/RentaTools/src/main/java/com/rentatools/RentaTools/exceptions/ResourceNotFoundException.java
+++ Backend/RentaTools/src/main/java/com/rentatools/RentaTools/exceptions/ResourceNotFoundException.java
@@ -1,5 +1,5 @@
 package com.rentatools.RentaTools.exceptions;
 
-public class ResourceNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException {
     public  ResourceNotFoundException(String message){super(message);}
 }
