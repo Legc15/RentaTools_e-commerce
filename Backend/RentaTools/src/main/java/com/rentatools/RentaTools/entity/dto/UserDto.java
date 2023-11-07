27914a064a8979ee8185897ecb014e1b7867057d
PabloKonig <pablokonig@yahoo.com>
2023-11-06 21:58:42 -0300

    Actualizado hasta el product/update

--- Backend/RentaTools/src/main/java/com/rentatools/RentaTools/entity/dto/UserDto.java
+++ Backend/RentaTools/src/main/java/com/rentatools/RentaTools/entity/dto/UserDto.java
@@ -1,20 +1,25 @@
 package com.rentatools.RentaTools.entity.dto;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Setter
-@Getter
 public class UserDto {
     @Column(nullable = false)
     private String name;
     private String lastName;
     @Column(nullable = false, unique = true)
+    @Email
+    @NotBlank
+    @Size(max = 80)
     private String email;
     @Column(nullable = false)
+    @NotBlank
     private String password;
     private boolean esAdmin = false;
     private boolean esActive = true;
