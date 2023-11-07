e674167b53841620d89792282198eaa97a82a868
Legc15 <leandro.garcia.casariego@gmail.com>
2023-11-07 15:52:22 -0300

    cambios varios

--- Frontend/RentaTools/src/pages/index.js
+++ Frontend/RentaTools/src/pages/index.js
@@ -3,5 +3,6 @@ import Register from "./Register"
 import Detail from "./Detail"
 import Home from "./Home"
 import SignUp from "./SignUp"
+import List from "./List" 
 
-export { Admin, Register, Detail, Home, SignUp }
+export { Admin, Register, Detail, Home, SignUp, List }
