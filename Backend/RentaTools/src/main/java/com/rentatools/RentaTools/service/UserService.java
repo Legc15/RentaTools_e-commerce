f093327a952d6e8964a82312240316992dc54709
PabloKonig <pablokonig@yahoo.com>
2023-11-06 21:58:42 -0300

    Actualizado hasta el product/update

--- Backend/RentaTools/src/main/java/com/rentatools/RentaTools/service/UserService.java
+++ Backend/RentaTools/src/main/java/com/rentatools/RentaTools/service/UserService.java
@@ -17,6 +17,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
