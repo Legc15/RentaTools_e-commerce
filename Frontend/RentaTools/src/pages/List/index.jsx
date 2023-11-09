20ac457261971793fee73f966ba2381f172a60b4
Legc15 <leandro.garcia.casariego@gmail.com>
2023-11-09 14:58:16 -0300

    feat: Task-8.1

--- Frontend/RentaTools/src/pages/List/index.jsx
+++ Frontend/RentaTools/src/pages/List/index.jsx
@@ -1,5 +1,5 @@
+
 import { useEffect, useState, useContext } from "react";
-import { useParams } from "react-router-dom";
 import { Searchbar } from "../../components/Searchbar";
 import { getInformationFromEndpoints } from "../../api/requestHandlers";
 import { ENDPOINTS_CODE } from "../../api/constants";
@@ -9,31 +9,44 @@ import ProductList  from "../../components/ProductList";
 import "./styles.css"
 
 const List = () => {
-    const { categoryId } = useParams();
     const { productsList, productsAll } = useContext(ContextGlobal);
     const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState(0);
-    const itemsPerPage = 10;
-
-
+    
     useEffect(() => {
-            getInformationFromEndpoints(ENDPOINTS_CODE.PRODUCTS_ALL,"", categoryId, page, itemsPerPage).then((response) => {
-                productsAll(response);
+        
+            getInformationFromEndpoints(ENDPOINTS_CODE.PRODUCTS_PAGINATED, {page})
+            .then((response) => {
+                productsAll(response.data);
                 setTotalPages(response.totalPages);
             });
-    }, [productsAll, categoryId, page, itemsPerPage]);
 
-    const handleChange = (event, value) => {
-        setPage(value);
+    }, [page]);
+
+
+    const handleChange = (e,v) => {
+        setPage(v)
     };
-
-    
-
     
     return (
-        <div className="body home-container  page-container listView">
-            <Searchbar />
-            <ProductList products={productsList}/>
+        <div className="body home-container  page-container prueba">
+            <div className="searchbar">
+                <Searchbar/>
+            
+                <Pagination 
+                    count={totalPages} 
+                    variant="outlined" 
+                    shape="rounded" 
+                    page={page} 
+                    onChange={handleChange} />
+            </div>
+            
+            
+            {productsList? 
+                <ProductList 
+                    products={productsList}
+                    className= "listView"    
+                        /> : 'Cargando productos' }
             <Pagination 
                 count={totalPages} 
                 variant="outlined" 
