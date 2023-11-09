c72a4a68ce8af6e82f44dc61bd1e7ca80c653cbe
PabloKonig <pablokonig@yahoo.com>
2023-11-09 18:49:33 -0300

    Backend hasta DB de características

--- Backend/RentaTools/src/main/java/com/rentatools/RentaTools/repository/IFeatureRepository.java
+++ Backend/RentaTools/src/main/java/com/rentatools/RentaTools/repository/IFeatureRepository.java
@@ -2,6 +2,7 @@ package com.rentatools.RentaTools.repository;
 import com.rentatools.RentaTools.entity.Feature;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
