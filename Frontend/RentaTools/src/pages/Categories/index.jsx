430209766be040ce7a60f0b393fb9e042685c83a
Ignacio Maza <Ignacio1445@icloud.com>
2023-11-23 10:23:29 -0300

    WIP: 8c2ba84 Merge pull request #62 from Legc15/Favoritos

--- Frontend/RentaTools/src/pages/Categories/index.jsx
+++ Frontend/RentaTools/src/pages/Categories/index.jsx
@@ -3,6 +3,9 @@ import "./styles.css"
 import { useNavigate } from "react-router-dom"
 import CategoriesForm from "../../components/Forms/CategoriesForm"
 import CategoriesTable from "../../components/Tables/CategoriesTable"
+import { useEffect, useState } from "react"
+import { getInformationFromEndpoints } from "../../api/requestHandlers"
+import { ENDPOINTS_CODE } from "../../api/constants"
 
 const Categories = () => {
   const navigate = useNavigate()
@@ -11,17 +14,26 @@ const Categories = () => {
     navigate("/admin")
   }
 
+  const [categories, setCategories] = useState([])
+
+
+  useEffect(() => {
+    getInformationFromEndpoints(ENDPOINTS_CODE.CATEGORY_ALL).then((response) => setCategories(response))
+  })
+
+
   return (
     <div className="body padding">
       <div>
+
         <Button variant="contained" onClick={navigateToAdmin} className="button">
           Regresar al Admin
         </Button>
       </div>
       <h1>Administrar Categorias</h1>
       <div className="information">
-        <CategoriesTable></CategoriesTable>
         <CategoriesForm></CategoriesForm>
+        <CategoriesTable categories={categories} />
       </div>
     </div>
   )
