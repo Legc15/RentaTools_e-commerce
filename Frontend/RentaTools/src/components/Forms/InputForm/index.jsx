1c5bc2427d991a54b77e50a18fe92f0e7853bc6d
mmir22 <marcelomiraballes@gmail.com>
2023-11-28 15:50:55 -0300

    se fixea boton registo y se redirecciona a login

--- Frontend/RentaTools/src/components/Forms/InputForm/index.jsx
+++ Frontend/RentaTools/src/components/Forms/InputForm/index.jsx
@@ -8,59 +8,75 @@ import { ENDPOINTS_CODE } from '../../../api/constants';
 
 export default function InputForm() {
 
- const [formData, setFormData] = useState({
-  name: '',
-  lastName: '',
-  email: '',
-  password: ''
- });
+  const [formData, setFormData] = useState({
+    name: '',
+    lastName: '',
+    email: '',
+    password: ''
+  });
 
- const [isFormCorrect, setIsFormCorrect] = useState(false)
- const [isFormSent, setIsFormSent] = useState(false)
+  const [isFormCorrect, setIsFormCorrect] = useState(false)
+  const [isFormSent, setIsFormSent] = useState(false)
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
 
- const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+  const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
 
-const validarFormulario = async() =>{
-  setIsFormSent(true)
-  if(formData.name.trim().length > 3 && formData.lastName.trim().length > 3 && formData.password.trim().length > 4 && emailRegex.test(formData.email)){
-    const response = await postNewInformation(ENDPOINTS_CODE.USER_CREATE, formData)
-  if (response.status === 200) { 
-    setIsFormCorrect(true)
+  const validarFormulario = async () => {
+    setIsFormSent(true)
+
+
+    if (
+      formData.name.trim().length > 3 &&
+      formData.lastName.trim().length > 3 &&
+      formData.password.trim().length > 4 &&
+      emailRegex.test(formData.email)
+    ) {
+      const response = await postNewInformation(ENDPOINTS_CODE.USER_CREATE, formData)
+      if (response.status === 200) {
+        setIsFormCorrect(true);
+        setIsButtonDisabled(true);
+
+        setTimeout(() => {
+          location.assign("/signIn")
+        }, 2000)
+      }else {
+        setIsButtonDisabled(false);
+      }
+    } 
   }
-}}
 
 
-const handleInputChange = (e) =>{
-  setFormData({...formData, [e.target.name]:e.target.value});
-}
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
 
-const handleSubmit = (e) =>{
-  e.preventDefault();
-  validarFormulario();
-
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await validarFormulario();
+    
+  }
+  
 
   return (
     <form className="formulario" onSubmit={handleSubmit}>
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        '& > :not(style)': { m: 1 },
-        flexDirection: "column"
-      }}
-    >
-      <TextField label= "Nombre" type='text' name='name' onChange={handleInputChange}/>
-      <TextField label= "Apellido" type='text' name='lastName' onChange={handleInputChange}/>
-      <TextField label= "Correo Electronico" type='text' name='email' onChange={handleInputChange} />
-      <TextField label= "Contraseña" type="password" name='password' onChange={handleInputChange} />
-    </Box>
-    {isFormSent && !isFormCorrect ? <div className="error-message">Uno o mas campos estan incorrectos.</div>: ""}    
-    {isFormSent && isFormCorrect ? <div className="success-message">Usuario registrado correctamente.</div> : ""}   
-     <HeaderButton buttonLabel="Registrar" className="registrar" type="submit"/>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          '& > :not(style)': { m: 1 },
+          flexDirection: "column"
+        }}
+      >
+        <TextField label="Nombre" type='text' name='name' onChange={handleInputChange} />
+        <TextField label="Apellido" type='text' name='lastName' onChange={handleInputChange} />
+        <TextField label="Correo Electronico" type='text' name='email' onChange={handleInputChange} />
+        <TextField label="Contraseña" type="password" name='password' onChange={handleInputChange} />
+      </Box>
+      {isFormSent && !isFormCorrect ? <div className="error-message">Uno o mas campos estan incorrectos.</div> : ""}
+      {isFormSent && isFormCorrect ? <div className="success-message">Usuario registrado correctamente. Redirigiendo...</div> : ""}
+      <HeaderButton buttonLabel="Registrar" className="registrar" type="submit" disabled={isButtonDisabled} />
     </form>
 
   )
 }
-
