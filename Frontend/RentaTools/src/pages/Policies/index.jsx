86a20c8e47abcc86c2f0608b63d51ca6087894c5
marianobonet~ <mariano-bonet@hotmail.com>
2023-12-01 10:44:45 -0300

    arreglo boton back policies

--- Frontend/RentaTools/src/pages/Policies/index.jsx
+++ Frontend/RentaTools/src/pages/Policies/index.jsx
@@ -3,30 +3,25 @@ import "./styles.css";
 const Policies = () => {
   return (
     <>
-      <div className="terms-top">
-        <button
-          className="button-Back"
-          onClick={() => window.history.back()}
-        >
+      <div className="policies-top">
+        <button className="button-Back" onClick={() => window.history.back()}>
           Volver Atras
         </button>
       </div>
 
-      <div className="termsBody">
-        <div></div>
+      <div className="policiesBody">
+        <div className="policies">
+          <h1 className="policies-title">Términos y condiciones</h1>
 
-        <div className="terms">
-          <h1 className="terms-title">Términos y condiciones</h1>
-
-          <p className="terms-discrecion">
+          <p className="policies-discrecion">
             Al acceder y utilizar nuestro sitio web de alquiler de maquinaria y
             herramientas para construcción, aceptas cumplir y estar sujeto a
             estos términos y condiciones de uso.
           </p>
           <ul>
             <li>
-              <h2 className="terms-bullet">Uso de la Plataforma</h2>
-              <p className="terms-descripcion">
+              <h2 className="policies-bullet">Uso de la Plataforma</h2>
+              <p className="policies-descripcion">
                 Nuestro sitio web proporciona una plataforma para alquilar
                 maquinaria y herramientas para construcción. Te comprometes a
                 utilizar nuestros servicios solo con fines legales y de acuerdo
@@ -34,8 +29,8 @@ const Policies = () => {
               </p>
             </li>
             <li>
-              <h2 className="terms-bullet">Registro y Cuentas de Usuario</h2>
-              <p className="terms-descripcion">
+              <h2 className="policies-bullet">Registro y Cuentas de Usuario</h2>
+              <p className="policies-descripcion">
                 Para utilizar ciertas funciones de nuestro sitio, puedes
                 necesitar crear una cuenta. Eres responsable de mantener la
                 confidencialidad de tu información de inicio de sesión y de
@@ -43,8 +38,8 @@ const Policies = () => {
               </p>
             </li>
             <li>
-              <h2 className="terms-bullet">Alquiler y Devolución</h2>
-              <p className="terms-descripcion">
+              <h2 className="policies-bullet">Alquiler y Devolución</h2>
+              <p className="policies-descripcion">
                 El proceso de alquiler de maquinaria y herramientas está sujeto
                 a disponibilidad y a términos específicos que se detallarán
                 durante el proceso de reserva. Te comprometes a devolver los
@@ -52,8 +47,8 @@ const Policies = () => {
               </p>
             </li>
             <li>
-              <h2 className="terms-bullet">Tarifas y Pagos</h2>
-              <p className="terms-descripcion">
+              <h2 className="policies-bullet">Tarifas y Pagos</h2>
+              <p className="policies-descripcion">
                 Las tarifas de alquiler se mostrarán claramente durante el
                 proceso de reserva. Aceptas pagar todas las tarifas y costos
                 asociados al uso de nuestro servicio según lo acordado. Nos
@@ -62,8 +57,8 @@ const Policies = () => {
               </p>
             </li>
             <li>
-              <h2 className="terms-bullet">Responsabilidades del Usuario</h2>
-              <p className="terms-descripcion">
+              <h2 className="policies-bullet">Responsabilidades del Usuario</h2>
+              <p className="policies-descripcion">
                 Eres responsable del uso adecuado y seguro de la maquinaria y
                 herramientas alquiladas. Te comprometes a utilizarlos siguiendo
                 las instrucciones proporcionadas y a cumplir con todas las
@@ -71,8 +66,8 @@ const Policies = () => {
               </p>
             </li>
             <li>
-              <h2 className="terms-bullet">Limitación de Responsabilidad</h2>
-              <p className="terms-descripcion">
+              <h2 className="policies-bullet">Limitación de Responsabilidad</h2>
+              <p className="policies-descripcion">
                 No nos hacemos responsables de cualquier daño, pérdida o lesión
                 resultante del uso de la maquinaria y herramientas
                 proporcionadas. Es responsabilidad del usuario utilizar los
@@ -80,8 +75,8 @@ const Policies = () => {
               </p>
             </li>
             <li>
-              <h2 className="terms-bullet">Propiedad Intelectual</h2>
-              <p className="terms-descripcion">
+              <h2 className="policies-bullet">Propiedad Intelectual</h2>
+              <p className="policies-descripcion">
                 Nuestro sitio web y su contenido (salvo que se indique lo
                 contrario) están protegidos por derechos de autor, marcas
                 registradas y otras leyes de propiedad intelectual. No puedes
@@ -89,10 +84,10 @@ const Policies = () => {
               </p>
             </li>
             <li>
-              <h2 className="terms-bullet">
+              <h2 className="policies-bullet">
                 Modificaciones de los Términos y Condiciones
               </h2>
-              <p className="terms-descripcion">
+              <p className="policies-descripcion">
                 Nos reservamos el derecho de modificar estos términos y
                 condiciones en cualquier momento. Se te notificará sobre
                 cualquier cambio significativo en estos términos. El uso
@@ -101,8 +96,8 @@ const Policies = () => {
               </p>
             </li>
             <li>
-              <h2 className="terms-bullet">Ley Aplicable</h2>
-              <p className="terms-descripcion">
+              <h2 className="policies-bullet">Ley Aplicable</h2>
+              <p className="policies-descripcion">
                 Estos términos y condiciones se rigen e interpretan de acuerdo
                 con las leyes vigentes de [indica la jurisdicción aplicable].
               </p>
