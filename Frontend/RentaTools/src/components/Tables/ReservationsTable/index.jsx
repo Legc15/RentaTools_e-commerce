db97dc23ba8fb117213ce7c06cf2eee35333713f
Legc15 <leandro.garcia.casariego@gmail.com>
2023-12-03 23:36:26 -0300

    refactor:  Reservation (Filter buttons)

--- Frontend/RentaTools/src/components/Tables/ReservationsTable/index.jsx
+++ Frontend/RentaTools/src/components/Tables/ReservationsTable/index.jsx
@@ -7,37 +7,84 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Button } from "@mui/material";
 import "./styles.css";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getReservationsByUserEndpoint } from "/src/api/endpoints";
 import { getUserId } from "../../../utils/localStorageHandler";
+import Swal from "sweetalert2/dist/sweetalert2";
+import Typography from "@mui/material/Typography";
 
 const ReservationsTable = () => {
   const id = getUserId();
-  console.log("User ID:", id);
   const [userReservations, setUserReservations] = useState([]);
+  const [dateFilter, setDateFilter] = useState("asc"); // 'asc' para ascendente, 'desc' para descendente
+
+  const handleCancelReserve = () => {
+    Swal.fire({
+      text: "Esta seguro que desea cancelar esta reserva?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Confirmar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+      }
+    });
+  };
+
+  const toggleDateFilter = () => {
+    setDateFilter((prevFilter) => (prevFilter === "asc" ? "desc" : "asc"));
+  };
 
   useEffect(() => {
     const fetchReservations = async () => {
       try {
         const userRes = await fetch(getReservationsByUserEndpoint({ id }));
-        const userResData = await userRes.json();
-        console.log("User reservations data:", userResData); // Log the response data
+        let userResData = await userRes.json();
         if (Array.isArray(userResData)) {
-          // Check if the response data is an array
+          // Ordena las reservaciones por fecha
+          userResData = userResData.sort((a, b) => {
+            const dateA = new Date(a.reservationFrom);
+            const dateB = new Date(b.reservationFrom);
+
+            return dateFilter === "asc" ? dateA - dateB : dateB - dateA;
+          });
+
           setUserReservations(userResData);
-        } else {
-          console.error("Expected an array but received:", userResData);
         }
       } catch (error) {
-        console.error("Error fetching reservations:", error);
+        // Maneja el error
       }
     };
 
     fetchReservations();
-  }, [id]);
+  }, [id, dateFilter]); // Agrega dateFilter como dependencia para que el efecto se ejecute cuando cambie
+
+  const [filter, setFilter] = useState('all'); // 'all' para todas las reservas, 'active' para reservas activas, 'finished' para reservas finalizadas
+
+  const handleFilterChange = (newFilter) => {
+    setFilter(newFilter);
+  }
 
   return (
-    <TableContainer component={Paper} className="table-container">
+    <TableContainer component={Paper} className="table-Container">
+      <div className="filter-buttons">
+        <Button onClick={toggleDateFilter} variant="contained" color="primary">
+          {dateFilter === "asc" ? "Orden Desc." : "Orden Ascendente"}
+        </Button>
+
+        <Button onClick={() => handleFilterChange('active')} variant="contained" color="primary">
+          reservas activas
+        </Button>
+
+        <Button onClick={() => handleFilterChange('finished')} variant="contained" color="primary">
+          reservas finalizadas
+        </Button>
+        <Button onClick={() => handleFilterChange('all')} variant="contained" color="primary">
+          Limpiar filtros
+        </Button>
+      </div>
       <Table sx={{ minWidth: 200 }} aria-label="simple table">
         <TableHead></TableHead>
         <TableHead>
@@ -46,21 +93,29 @@ const ReservationsTable = () => {
               <h3>ID</h3>
             </TableCell>
             <TableCell align="left">
-              <h3>Producto</h3>
+              <h3>PRODUCTO</h3>
             </TableCell>
             <TableCell align="center" className="table-header">
-              Desde
+              <h3>DESDE</h3>
             </TableCell>
             <TableCell align="center" className="table-header">
-              Hasta
+              <h3>HASTA</h3>
             </TableCell>
             <TableCell align="center" className="table-header">
-              Accion
+              <h3>ESTADO</h3>
+            </TableCell>
+            <TableCell align="center" className="table-header">
+              <h3>ACCIÓN</h3>
             </TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
-          {userReservations.map((reservation, index) => (
+        <TableBody className="table-body">
+
+          {userReservations.filter(reservation => {
+            if (filter === 'all') return true;
+            if (filter === 'active') return new Date(reservation.reservationTo) > new Date();
+            if (filter === 'finished') return new Date(reservation.reservationTo) <= new Date();
+          }).map((reservation, index) => (
             <TableRow
               key={index}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -72,9 +127,29 @@ const ReservationsTable = () => {
               </TableCell>
               <TableCell align="center">{reservation.reservationTo}</TableCell>
               <TableCell align="center">
-                <Button variant="contained" color="error">
-                  Cancelar Reserva
-                </Button>
+                {new Date(reservation.reservationTo) > new Date()
+                  ? (
+                    <Typography color="green" variant="h6">
+                      ACTIVO
+                    </Typography>)
+                  : (
+                    <Typography color="grey" variant="subtitle2">
+                      FINALIZADA
+                    </Typography>)}
+              </TableCell>
+
+              <TableCell align="center">
+                {new Date(reservation.reservationTo) > new Date() ? (
+                  <Button
+                    onClick={handleCancelReserve}
+                    variant="contained"
+                    color="error"
+                  >
+                    Cancelar Reserva
+                  </Button>
+                ) : (
+                  "-----"
+                )}
               </TableCell>
             </TableRow>
           ))}
