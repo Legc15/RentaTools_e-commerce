b2d40749e4f113075dd7c77c1d9e8bfe8a448ae8
Legc15 <leandro.garcia.casariego@gmail.com>
2023-12-04 18:50:14 -0300

    refactor: styles Reservations

--- Frontend/RentaTools/src/components/Tables/ReservationsTable/index.jsx
+++ Frontend/RentaTools/src/components/Tables/ReservationsTable/index.jsx
@@ -10,27 +10,38 @@ import "./styles.css";
 import { useState, useEffect } from "react";
 import { getReservationsByUserEndpoint } from "/src/api/endpoints";
 import { getUserId } from "../../../utils/localStorageHandler";
-import Swal from "sweetalert2/dist/sweetalert2";
 import Typography from "@mui/material/Typography";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 const ReservationsTable = () => {
   const id = getUserId();
   const [userReservations, setUserReservations] = useState([]);
   const [dateFilter, setDateFilter] = useState("desc"); // 'asc' para ascendente, 'desc' para descendente
 
-  const handleCancelReserve = () => {
-    Swal.fire({
-      text: "Esta seguro que desea cancelar esta reserva?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#d33",
-      cancelButtonColor: "#3085d6",
-      confirmButtonText: "Confirmar",
-      cancelButtonText: "Cancelar",
-    }).then((result) => {
-      if (result.isConfirmed) {
-      }
-    });
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleConfirm = () => {
+    // Aquí va el código para confirmar la reserva
+    handleClose();
+  };
+
+  const handleCancel = () => {
+    // Aquí va el código para cancelar la reserva
+    handleClose();
   };
 
   const toggleDateFilter = () => {
@@ -61,114 +72,197 @@ const ReservationsTable = () => {
     fetchReservations();
   }, [id, dateFilter]); // Agrega dateFilter como dependencia para que el efecto se ejecute cuando cambie
 
-  const [filter, setFilter] = useState('active'); // 'all' para todas las reservas, 'active' para reservas activas, 'finished' para reservas finalizadas
+  const [filter, setFilter] = useState("active"); // 'all' para todas las reservas, 'active' para reservas activas, 'finished' para reservas finalizadas
 
   const handleFilterChange = (newFilter) => {
     setFilter(newFilter);
-  }
+  };
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  //const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
 
   return (
-    <TableContainer component={Paper} className="table-Container">
+    <TableContainer omponent={Paper} className="table-Container">
       <div className="filter-buttons">
-        <Button onClick={toggleDateFilter} variant="contained" color="primary">
-          {dateFilter === "asc" ? "Orden Ascendente" : "Orden descendente"}
-        </Button>
 
-        <Button onClick={() => handleFilterChange('active')} 
-          variant="contained" 
-          color="primary" 
-          style={filter === 'active' ? { backgroundColor: 'green' } : {}}
+
+        <Button
+          onClick={() => handleFilterChange("active")}
+          variant="contained"
+          color="primary"
+          style={filter === "active" ? { backgroundColor: "green" } : {}}
+          size={isMobile ? "small" : "medium"}
         >
           reservas activas
         </Button>
 
-        <Button onClick={() => handleFilterChange('finished')} 
-          variant="contained" 
+        <Button
+          onClick={() => handleFilterChange("finished")}
+          variant="contained"
           color="primary"
-          style={filter === 'finished' ? { backgroundColor: 'green' } : {}}
-        
-          >
+          style={filter === "finished" ? { backgroundColor: "green" } : {}}
+          size={isMobile ? "small" : "medium"}
+        >
           reservas finalizadas
         </Button>
-        <Button onClick={() => handleFilterChange('all')} 
-          variant="contained" 
+        <Button
+          onClick={() => handleFilterChange("all")}
+          variant="contained"
           color="primary"
-          style={filter === 'all' ? { backgroundColor: 'green' } : {}}
-          >
-          HISTORIAL COMPLETO
+          style={filter === "all" ? { backgroundColor: "green" } : {}}
+          size={isMobile ? "small" : "medium"}
+        >
+          todas las reservas
+        </Button>
+
+        <Button onClick={toggleDateFilter} variant="contained" color="primary">
+          {dateFilter === "asc" ? "Orden Ascendente" : "Orden  descendente"}
         </Button>
       </div>
-      <Table sx={{ minWidth: 200 }} aria-label="simple table">
-        
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">
-              <h3>ID</h3>
-            </TableCell>
-            <TableCell align="left">
-              <h3>PRODUCTO</h3>
-            </TableCell>
-            <TableCell align="center" className="table-header">
-              <h3>DESDE</h3>
-            </TableCell>
-            <TableCell align="center" className="table-header">
-              <h3>HASTA</h3>
-            </TableCell>
-            <TableCell align="center" className="table-header">
-              <h3>ESTADO</h3>
-            </TableCell>
-            <TableCell align="center" className="table-header">
-              <h3>ACCIÓN</h3>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody className="table-body">
 
-          {userReservations.filter(reservation => {
-            if (filter === 'all') return true;
-            if (filter === 'active') return new Date(reservation.reservationTo) > new Date();
-            if (filter === 'finished') return new Date(reservation.reservationTo) <= new Date();
-          }).map((reservation, index) => (
-            <TableRow
-              key={index}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              style={new Date(reservation.reservationTo) > new Date() ? { backgroundColor: 'whitesmoke' } : {}}
-            >
-              <TableCell align="center">{reservation.id}</TableCell>
-              <TableCell align="left">{reservation.product.name}</TableCell>
+        <Table
+          className="tabla-reservas"
+          sx={{ minWidth: 200, margin: isMobile ? "0" : "auto" }}
+          aria-label="simple table"
+          style={{ display: 'block', maxHeight: '500px', overflowY: 'auto' }}
+        >
+          <TableHead>
+            <TableRow>
               <TableCell align="center">
-                {reservation.reservationFrom}
+                <h3>ID</h3>
               </TableCell>
-              <TableCell align="center">{reservation.reservationTo}</TableCell>
-              <TableCell align="center">
-                {new Date(reservation.reservationTo) > new Date()
-                  ? (
-                    <Typography className="active" color="green" variant="h6" style={{ fontWeight: 600 }}>
-                      ACTIVO
-                    </Typography>)
-                  : (
-                    <Typography color="grey" variant="subtitle2">
-                      FINALIZADA
-                    </Typography>)}
+              <TableCell align="left">
+                <h3>PRODUCTO</h3>
               </TableCell>
-
-              <TableCell align="center">
-                {new Date(reservation.reservationTo) > new Date() ? (
-                  <Button
-                    onClick={handleCancelReserve}
-                    variant="contained"
-                    color="error"
-                  >
-                    Cancelar Reserva
-                  </Button>
-                ) : (
-                  "-----"
-                )}
+              <TableCell align="center" className="table-header">
+                <h3>DESDE</h3>
+              </TableCell>
+              <TableCell align="center" className="table-header">
+                <h3>HASTA</h3>
+              </TableCell>
+              <TableCell align="center" className="table-header">
+                <h3>ESTADO</h3>
+              </TableCell>
+              <TableCell align="center" className="table-header">
+                <h3>ACCIÓN</h3>
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody className="table-body">
+            {userReservations
+              .filter((reservation) => {
+                if (filter === "all") return true;
+                if (filter === "active")
+                  return new Date(reservation.reservationTo) > new Date();
+                if (filter === "finished")
+                  return new Date(reservation.reservationTo) <= new Date();
+              })
+              .map((reservation, index) => (
+                <TableRow
+                  key={index}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  style={
+                    new Date(reservation.reservationTo) > new Date()
+                      ? { backgroundColor: "whitesmoke" }
+                      : {}
+                  }
+                >
+                  <TableCell
+                    align="center"
+                    style={{ fontSize: isMobile ? "10px" : "14px" }}
+                  >
+                    {reservation.id}
+                  </TableCell>
+                  <TableCell
+                    align="left"
+                    style={{ fontSize: isMobile ? "10px" : "14px" }}
+                  >
+                    {reservation.product.name}
+                  </TableCell>
+                  <TableCell
+                    align="center"
+                    style={{ fontSize: isMobile ? "10px" : "14px" }}
+                  >
+                    {reservation.reservationFrom}
+                  </TableCell>
+                  <TableCell
+                    align="center"
+                    style={{ fontSize: isMobile ? "10px" : "14px" }}
+                  >
+                    {reservation.reservationTo}
+                  </TableCell>
+                  <TableCell
+                    align="center"
+                    style={{ fontSize: isMobile ? "10px" : "14px" }}
+                  >
+                    {new Date(reservation.reservationTo) > new Date() ? (
+                      <Typography
+                        className="active"
+                        color="green"
+                        style={{ fontWeight: isMobile ? 300 : 500 }}
+                        variant={"subtitle2"}
+                      >
+                        ACTIVO
+                      </Typography>
+                    ) : (
+                      <Typography color="grey" variant="subtitle2">
+                        FINALIZADA
+                      </Typography>
+                    )}
+                  </TableCell>
+
+                  <TableCell align="center">
+                    {new Date(reservation.reservationTo) > new Date() ? (
+                      <Button
+                        style={{
+                          fontSize: isMobile ? "15px" : "initial",
+                          borderRadius: isMobile ? "50%" : "4px",
+                          height: isMobile ? "25px" : "initial",
+                          width: isMobile ? "25px" : "initial",
+                          padding: "4px",
+                          margin: "5px",
+                          minWidth: isMobile ? "0" : "initial", // Esto es para anular el ancho mínimo predeterminado de los botones de Material-UI
+                        }}
+                        onClick={handleClickOpen}
+                        variant="contained"
+                      >
+                        {isMobile ? " + " : "modificar"}
+                      </Button>
+                    ) : (
+                      "-----"
+                    )}
+                    <Dialog
+                      open={open}
+                      onClose={handleClose}
+                      aria-labelledby="alert-dialog-title"
+                      aria-describedby="alert-dialog-description"
+                    >
+                      <DialogTitle id="alert-dialog-title">
+                        {"Elija una opción"}
+                      </DialogTitle>
+                      <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                          Para salir presione fuera del recuadro
+                        </DialogContentText>
+                      </DialogContent>
+                      <DialogActions>
+                        <Button onClick={handleCancel} color="primary">
+                          Cancelar reserva
+                        </Button>
+                        <Button
+                          onClick={handleConfirm}
+                          color="primary"
+                          autoFocus
+                        >
+                          Modificar reserva
+                        </Button>
+                      </DialogActions>
+                    </Dialog>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
     </TableContainer>
   );
 };
